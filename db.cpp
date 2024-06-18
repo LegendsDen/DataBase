78e212281e889b39d1243ffb2e1e6f3af3f2503d
Sushant <sushant3660jee@gmail.com>
2024-06-18 23:22:13 +0530

    A simple Virtual Machine Added

--- db.cpp
+++ db.cpp
@@ -9,6 +9,24 @@ struct InputBuffer {
     size_t buffer_length;
     ssize_t input_length;
 };
+enum MetaCommandResult {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+};
+
+enum PrepareResult {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+};
+
+enum StatementType {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+};
+
+struct Statement {
+    StatementType type;
+};
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -40,6 +58,40 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+            std::cout << "This is where we would do an insert.\n";
+            break;
+        case (STATEMENT_SELECT):
+            std::cout << "This is where we would do a select.\n";
+            break;
+    }
+}
+
+
 
 
 int main(int argc, char* argv[]) {
@@ -49,11 +101,26 @@ int main(int argc, char* argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            cout << "Unrecognized command '" << input_buffer->buffer << "'.\n";
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    std::cout << "Unrecognized command '" << input_buffer->buffer << "'\n";
+                    continue;
+            }
         }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                std::cout << "Unrecognized keyword at start of '" << input_buffer->buffer << "'.\n";
+                continue;
+        }
+
+        execute_statement(&statement);
+        std::cout << "Executed.\n";
     }
 }
